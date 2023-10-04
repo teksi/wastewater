6fc968dfc76599da8ae5a8d9cde2d70531140a41
SJiB <stefan.burckhardt@gmx.ch>
2023-10-04 16:57:54 +0200

    00_tww_schema.sql instead of 00_qgep_schema

---
+++ datamodel/00_tww_schema.sql
@@ -0,0 +1,12 @@
+------ This file generates the VSA-DSS database (Modul VSA-DSS (2020)) in en on QQIS
+------ For questions etc. please contact Stefan Burckhardt stefan.burckhardt@sjib.ch
+------ version 04.10.2023 16:38:12
+------ with 3D coordinates
+BEGIN;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE SCHEMA tww_sys;
+CREATE SCHEMA tww_vl;
+CREATE SCHEMA tww_od;
+CREATE SCHEMA tww_import;
+CREATE SCHEMA tww_network;
