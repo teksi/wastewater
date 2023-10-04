61ffcdf25ca391449869919a50354d3d46d3b11e
SJiB <stefan.burckhardt@gmx.ch>
2023-10-04 20:16:59 +0200

    Adapt rename_damage_subclass_attributes.sql
    Only temporary - should be solved directly with datamodel.

--- datamodel/rename_damage_subclass_attributes.sql
+++ datamodel/rename_damage_subclass_attributes.sql
@@ -2,34 +2,34 @@
 
 -- damage_channel
 
-ALTER TABLE qgep_od.damage_channel 
+ALTER TABLE tww_od.damage_channel 
 RENAME COLUMN distance TO channel_distance;
 
-ALTER TABLE qgep_od.damage_channel 
+ALTER TABLE tww_od.damage_channel 
 RENAME COLUMN damage_begin TO channel_damage_begin;
 
-ALTER TABLE qgep_od.damage_channel 
+ALTER TABLE tww_od.damage_channel 
 RENAME COLUMN damage_end TO channel_damage_end;
 
-ALTER TABLE qgep_od.damage_channel 
+ALTER TABLE tww_od.damage_channel 
 RENAME COLUMN quantification1  TO channel_quantification1;
 
-ALTER TABLE qgep_od.damage_channel 
+ALTER TABLE tww_od.damage_channel 
 RENAME COLUMN quantification2  TO channel_quantification2;
 
 -- damage_manhole
 
-ALTER TABLE qgep_od.damage_manhole 
+ALTER TABLE tww_od.damage_manhole 
 RENAME COLUMN distance TO manhole_channel_distance;
 
-ALTER TABLE qgep_od.damage_manhole 
+ALTER TABLE tww_od.damage_manhole 
 RENAME COLUMN damage_begin TO manhole_damage_begin;
 
-ALTER TABLE qgep_od.damage_manhole 
+ALTER TABLE tww_od.damage_manhole 
 RENAME COLUMN damage_end TO manhole_damage_end;
 
-ALTER TABLE qgep_od.damage_manhole 
+ALTER TABLE tww_od.damage_manhole 
 RENAME COLUMN quantification1  TO manhole_quantification1;
 
-ALTER TABLE qgep_od.damage_manhole 
+ALTER TABLE tww_od.damage_manhole 
 RENAME COLUMN quantification2  TO manhole_quantification2;
