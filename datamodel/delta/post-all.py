320b669026adf5d9a87caf9f592beaea64eb1390
SJiB <stefan.burckhardt@gmx.ch>
2023-10-05 11:57:25 +0200

    replace qgep with tww

--- datamodel/delta/post-all.py
+++ datamodel/delta/post-all.py
@@ -11,19 +11,19 @@ from view.create_views import create_views
 class CreateViews(DeltaPy):
 
     def run(self):
-        qgep_wastewater_structure_extra = self.variables.get('qgep_wastewater_structure_extra', None)
-        qgep_reach_extra = self.variables.get('qgep_reach_extra', None)
+        tww_wastewater_structure_extra = self.variables.get('tww_wastewater_structure_extra', None)
+        tww_reach_extra = self.variables.get('tww_reach_extra', None)
 
         if not self.variables.get('SRID'):
             raise RuntimeError('SRID not specified. Add `-v int SRID 2056` (or the corresponding EPSG code) to the upgrade command.')
         create_views(srid=self.variables.get('SRID'),
                      pg_service=self.pg_service,
-                     qgep_wastewater_structure_extra=qgep_wastewater_structure_extra,
-                     qgep_reach_extra=qgep_reach_extra)
+                     tww_wastewater_structure_extra=tww_wastewater_structure_extra,
+                     tww_reach_extra=tww_reach_extra)
 
         # refresh network views
         conn = psycopg2.connect("service={0}".format(self.pg_service))
         cursor = conn.cursor()
-        cursor.execute('SELECT qgep_network.refresh_network_simple();')
+        cursor.execute('SELECT tww_network.refresh_network_simple();')
         conn.commit()
         conn.close()
