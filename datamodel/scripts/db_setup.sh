55736ff1c0eeaef74258c0b9973a9fdbc2efe1b2
SJiB <stefan.burckhardt@gmx.ch>
2023-10-05 13:58:09 +0200

    comment and adapt db_setup.sh

--- datamodel/scripts/db_setup.sh
+++ datamodel/scripts/db_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# This script will create a clean datastructure for the TEKSI Wastewater & GEP project based on
-# the VSA-DSS 2020 datamodel.
-# It will create a new schema tww in a postgres database.
+# This script will create a clean datastructure for the 
+# TEKSI Wastewater & GEP project
+# based on the VSA-DSS 2020 datamodel (see www.vsa.ch/model)
+# It will create new schemata tww_* in a postgres database.
 #
 # Environment variables:
 #
@@ -17,6 +18,7 @@
 # Exit on error
 set -e
 
+# set SRID
 SRID=2056
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
@@ -56,12 +58,15 @@ while getopts ":rfs:p:" opt; do
   esac
 done
 
+# clean database - drop schemata
 if [[ $force ]]; then
   psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "DROP SCHEMA IF EXISTS tww_sys CASCADE"
   psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "DROP SCHEMA IF EXISTS tww_od CASCADE"
   psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "DROP SCHEMA IF EXISTS tww_vl CASCADE"
   psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -c "DROP SCHEMA IF EXISTS tww_import CASCADE"
 fi
+
+# recreate datamodel
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/00_tww_schema.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/01_audit.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/02_oid_generation.sql
@@ -73,22 +78,31 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/07_networ
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/09_tww_dictionaries.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/09_tww_dictionaries_kek.sql
 
+# fixes
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/fixes/fix_wastewater_structure.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/fixes/fix_depth.sql
-psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/fixes/fix_od_file.sql
+# not needed anymore - already integrated in VSA-DSS 2020
+# psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/fixes/fix_od_file.sql
 
-psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/50_maintenance_zones.sql
+# extra tables from VSA-DSS 2015
+# extra feature from Koni - Is not VSA-DSS, so there is data loss when exporting with INTERLIS. These "Zones" or "Etappen" should be done with maintenance_events standard attributes
+# psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/50_maintenance_zones.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/51_aquifier_2015.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/52_planning_zone_2015.sql
 
+# extra function
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/functions/reach_direction_change.sql
 
+
+# import classes
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/13_import.sql
 
 
+# create views
 ${DIR}/view/create_views.py --pg_service ${PGSERVICE} --srid ${SRID}
 
 
+# add roles
 if [[ $roles ]]; then
   psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/12_0_roles.sql
 fi
@@ -96,8 +110,10 @@ if [[ $permissions ]]; then
   psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/12_1_roles.sql
 fi
 
+# set pum baseline
 VERSION=$(cat ${DIR}/system/CURRENT_VERSION.txt)
 pum baseline -p ${PGSERVICE} -t tww_sys.pum_info -d ${DIR}/delta/ -b ${VERSION}
 
 
+# add geometry functions
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/14_geometry_functions.sql
