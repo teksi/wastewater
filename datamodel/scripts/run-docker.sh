e0641d89213b6221ae59ec52bc02d36f2dbd2dcf
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-10-30 11:15:20 +0100

    cfg schema for swmm table + put back dictionnairies

--- datamodel/scripts/run-docker.sh
+++ datamodel/scripts/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -f .docker/Dockerfile --tag teksi/wastewater .
-docker run -d -p 5433:5432 -v $(pwd):/src  --name teksi-wastewater teksi/wastewater
+docker build -f datamodel/.docker/Dockerfile --tag teksi/wastewater .
+docker run -d -p 5433:5432 -v $(pwd)/datamodel:/src  --name teksi-wastewater teksi/wastewater
 docker exec teksi-wastewater init_db.sh wait
 docker exec teksi-wastewater init_db.sh build -d
