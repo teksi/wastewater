25e53b9ad5961ab5f7c1b038dc87909aa725204e
Damiano Lombardi <damiano@opengis.ch>
2023-11-16 20:39:28 +0100

    Skip most SWMM tests as they need demo data to work

--- datamodel/test/test_swmm.py
+++ datamodel/test/test_swmm.py
@@ -24,60 +24,76 @@ class TestSwmm(unittest.TestCase, DbTestBase):
     def test_count_vw_aquifers(self):
         self.assert_count("swmm_vw_aquifers", "tww_app", 0)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_conduits(self):
         self.assert_count("swmm_vw_conduits", "tww_app", 5095)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_coordinates(self):
         self.assert_count("swmm_vw_coordinates", "tww_app", 8044)
 
     def test_count_vw_coverages(self):
         self.assert_count("swmm_vw_coverages", "tww_app", 0)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_dividers(self):
         self.assert_count("swmm_vw_dividers", "tww_app", 45)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_dwf(self):
         self.assert_count("swmm_vw_dwf", "tww_app", 2035)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_infiltration(self):
         self.assert_count("swmm_vw_infiltration", "tww_app", 1352)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_junctions(self):
         self.assert_count("swmm_vw_junctions", "tww_app", 5864)
 
     def test_count_vw_landuses(self):
         self.assert_count("swmm_vw_landuses", "tww_app", 6)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_losses(self):
         self.assert_count("swmm_vw_losses", "tww_app", 5095)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_outfalls(self):
         self.assert_count("swmm_vw_outfalls", "tww_app", 54)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_polygons(self):
         self.assert_count("swmm_vw_polygons", "tww_app", 27405)
 
     def test_count_vw_pumps(self):
         self.assert_count("swmm_vw_pumps", "tww_app", 0)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_raingages(self):
         self.assert_count("swmm_vw_raingages", "tww_app", 2035)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_storages(self):
         self.assert_count("swmm_vw_storages", "tww_app", 46)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_subareas(self):
         self.assert_count("swmm_vw_subareas", "tww_app", 2035)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_subcatchments(self):
         self.assert_count("swmm_vw_subcatchments", "tww_app", 2035)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_tags(self):
         self.assert_count("swmm_vw_tags", "tww_app", 13094)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_vertices(self):
         self.assert_count("swmm_vw_vertices", "tww_app", 2854)
 
+    @unittest.skip("This test needs the demo data to work")
     def test_count_vw_xsections(self):
         self.assert_count("swmm_vw_xsections", "tww_app", 5095)
 
