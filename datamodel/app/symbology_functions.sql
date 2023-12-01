b3886c4c30cdd4576131f5bf3aae30f659e3af32
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-12-01 08:00:50 +0100

    add test

--- datamodel/app/symbology_functions.sql
+++ datamodel/app/symbology_functions.sql
@@ -693,74 +693,74 @@ BEGIN
   AFTER UPDATE
     ON tww_od.reach_point
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_reach_point_update();
+    EXECUTE PROCEDURE tww_app.on_reach_point_update();
 
   CREATE TRIGGER on_reach_2_change
   AFTER INSERT OR UPDATE OR DELETE
     ON tww_od.reach
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_reach_change();
+    EXECUTE PROCEDURE tww_app.on_reach_change();
 
   CREATE TRIGGER on_reach_1_delete
   AFTER DELETE
     ON tww_od.reach
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_reach_delete();
+    EXECUTE PROCEDURE tww_app.on_reach_delete();
 
   CREATE TRIGGER calculate_reach_length
   BEFORE INSERT OR UPDATE
     ON tww_od.reach
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.calculate_reach_length();
+    EXECUTE PROCEDURE tww_app.calculate_reach_length();
 
   CREATE TRIGGER ws_symbology_update_by_reach
   AFTER INSERT OR UPDATE OR DELETE
     ON tww_od.reach
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.ws_symbology_update_by_reach();
+    EXECUTE PROCEDURE tww_app.ws_symbology_update_by_reach();
 
   CREATE TRIGGER on_wastewater_structure_update
   AFTER UPDATE
     ON tww_od.wastewater_structure
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_wastewater_structure_update();
+    EXECUTE PROCEDURE tww_app.on_wastewater_structure_update();
 
   CREATE TRIGGER ws_label_update_by_wastewater_networkelement
   AFTER INSERT OR UPDATE OR DELETE
     ON tww_od.wastewater_networkelement
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_structure_part_change_networkelement();
+    EXECUTE PROCEDURE tww_app.on_structure_part_change_networkelement();
 
   CREATE TRIGGER on_structure_part_change
   AFTER INSERT OR UPDATE OR DELETE
     ON tww_od.structure_part
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_structure_part_change_networkelement();
+    EXECUTE PROCEDURE tww_app.on_structure_part_change_networkelement();
 
   CREATE TRIGGER on_cover_change
   AFTER INSERT OR UPDATE OR DELETE
     ON tww_od.cover
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_cover_change();
+    EXECUTE PROCEDURE tww_app.on_cover_change();
 
   CREATE TRIGGER on_wasterwaternode_change
   AFTER INSERT OR UPDATE
     ON tww_od.wastewater_node
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.on_wasterwaternode_change();
+    EXECUTE PROCEDURE tww_app.on_wasterwaternode_change();
 
   CREATE TRIGGER ws_symbology_update_by_channel
   AFTER INSERT OR UPDATE OR DELETE
   ON tww_od.channel
   FOR EACH ROW
-  EXECUTE PROCEDURE tww_od.ws_symbology_update_by_channel();
+  EXECUTE PROCEDURE tww_app.ws_symbology_update_by_channel();
 
   -- only update -> insert and delete are handled by reach trigger
   CREATE TRIGGER ws_symbology_update_by_reach_point
   AFTER UPDATE
     ON tww_od.reach_point
   FOR EACH ROW
-    EXECUTE PROCEDURE tww_od.ws_symbology_update_by_reach_point();
+    EXECUTE PROCEDURE tww_app.ws_symbology_update_by_reach_point();
 
 
   RETURN;
