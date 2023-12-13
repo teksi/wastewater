305d56543ffd271f330993eaf77d12435200a7cf
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-12-13 09:13:37 +0100

    explicit column definition

--- datamodel/app/view/vw_dictionary_value_list.sql
+++ datamodel/app/view/vw_dictionary_value_list.sql
@@ -1,5 +1,19 @@
 
 CREATE VIEW tww_app.sys_dictionary_value_list AS
-  SELECT p.relname AS vl_name, vl.*
-  FROM tww_vl.value_list_base vl, pg_class p
-  WHERE vl.tableoid = p.oid;
+  SELECT 
+  p.relname AS vl_name,
+  vl.code,
+  vl.vsacode,
+  vl.value_en,
+  vl.value_de,
+  vl.value_fr,
+  vl.value_it,
+  vl.value_ro,
+  vl.abbr_en,
+  vl.abbr_de,
+  vl.abbr_fr,
+  vl.abbr_it,
+  vl.abbr_ro,
+  vl.active
+FROM tww_vl.value_list_base vl, pg_class p
+WHERE vl.tableoid = p.oid;
