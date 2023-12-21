10ef2246720e296e7ec8c84e2157dfb40e362c28
Damiano Lombardi <damiano@opengis.ch>
2023-12-21 17:24:35 +0100

    Cleanup

--- plugin/teksi_wastewater/tww2ili/docs/stefans_scripts/export/check_coordinate_cover.sql
+++
@@ -1,3 +0,0 @@
-SELECT
-ST_AsText(situation_geometry) as deckelkoordinaten
-			FROM tww_od.cover ORDER BY deckelkoordinaten ASC;
--- plugin/teksi_wastewater/tww2ili/docs/stefans_scripts/export/check_length_reach.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-ST_AsText(progression_geometry) as koordinaten,
-            st_length(progression_geometry) AS length_full, obj_id
-			FROM tww_od.reach ORDER BY length_full ASC;
