0ab711b998d2aae689f451c636fa94b9e670ceac
Damiano Lombardi <damiano@opengis.ch>
2024-01-02 11:34:09 +0100

    First bits of plugin tests

--- plugin/interlis/gui/gui_import.py
+++ plugin/interlis/gui/gui_import.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 
 class GuiImport(QDialog):
-    def __init__(self, parent):
+    def __init__(self, parent=None):
         super().__init__(parent)
         loadUi(os.path.join(os.path.dirname(__file__), "gui_import.ui"), self)
 
---
+++ plugin/teksi_wastewater/tests/__init__.py
@@ -0,0 +1,1 @@
+from .test_interlis import *  # noqa
