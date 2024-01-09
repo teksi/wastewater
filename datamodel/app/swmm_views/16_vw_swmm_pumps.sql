d2652901b07da80b668130d30345dfe5ce878fc7
cymed <102035587+cymed@users.noreply.github.com>
2024-01-09 13:09:40 +0100

    update swmm views

--- datamodel/app/swmm_views/16_vw_swmm_pumps.sql
+++ datamodel/app/swmm_views/16_vw_swmm_pumps.sql
@@ -46,5 +46,5 @@ LEFT JOIN tww_vl.overflow_char_kind_overflow_char vl_oc_ki ON oc.kind_overflow_c
 LEFT JOIN tww_od.wastewater_node wn ON wn.obj_id = of.fk_wastewater_node
 LEFT JOIN tww_od.wastewater_structure ws ON ws.fk_main_wastewater_node = wn.obj_id
 LEFT JOIN tww_vl.wastewater_structure_status ws_st ON ws.status = ws_st.code
-LEFT JOIN tww_vl.channel_function_hierarchic cfhi ON cfhi.code=ws._function_hierarchic
+LEFT JOIN tww_vl.channel_function_hierarchic cfhi ON cfhi.code=wn._function_hierarchic
 WHERE ws_st.vsacode IN (6530, 6533, 8493, 6529, 6526, 7959);
