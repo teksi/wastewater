4035fc82ceb8ae1232fac9735520b315c22fe82c
cymed <102035587+cymed@users.noreply.github.com>
2024-01-15 12:00:35 +0100

    escape % with %% for update

--- datamodel/app/view/vw_tww_wastewater_structure.py
+++ datamodel/app/view/vw_tww_wastewater_structure.py
@@ -403,14 +403,14 @@ def vw_tww_wastewater_structure(srid: int, pg_service: str = None, extra_definit
 
       IF OLD.ws_type <> NEW.ws_type THEN
         CASE WHEN OLD.ws_type <> 'unknown' THEN
-          EXECUTE FORMAT('DELETE FROM tww_od.%I WHERE obj_id = %I',OLD.ws_type,OLD.obj_id);
+          EXECUTE FORMAT('DELETE FROM tww_od.%%I WHERE obj_id = %%I',OLD.ws_type,OLD.obj_id);
         END CASE;
 
         CASE WHEN NEW.ws_type <> 'unknown' THEN
-          EXECUTE FORMAT('INSERT INTO FROM tww_od.%I(obj_id) VALUES (%I)',NEW.ws_type,OLD.obj_id);
+          EXECUTE FORMAT('INSERT INTO FROM tww_od.%%I(obj_id) VALUES (%%I)',NEW.ws_type,OLD.obj_id);
           EXCEPTION
             WHEN undefined_table THEN
-              RAISE NOTICE 'table tww_od.% does not exist, rolling back',NEW.ws_type;
+              RAISE NOTICE 'table tww_od.%% does not exist, rolling back',NEW.ws_type;
               RETURN OLD;
           END;
         END CASE;
