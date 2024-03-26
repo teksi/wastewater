9c131cfb7794619a13574fe8f9ee983c96a0a308
Arnaud Poncet-Montanges <ponceta@users.noreply.github.com>
2024-03-26 15:48:12 +0100

    try import without app keyword

--- datamodel/app/create_tww_app.py
+++ datamodel/app/create_tww_app.py
@@ -4,7 +4,7 @@ import argparse
 import os
 
 import psycopg2
-from app.view.create_views import create_views
+from .view.create_views import create_views
 
 
 def run_sql_file(file_path: str, pg_service: str, variables: dict = {}):
