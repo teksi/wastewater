26d891b1e984bb6c864ee2a3cdeb00222183baec
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-04-09 18:02:58 +0200

    handle cursors

--- datamodel/test/test_removed_fields.py
+++ datamodel/test/test_removed_fields.py
@@ -5,6 +5,7 @@ try:
     import psycopg
 except ImportError:
     import psycopg2 as psycopg
+    import psycopg2.extras as psycopg_extras
 
 from .utils import DEFAULT_PG_SERVICE, DbTestBase
 
@@ -20,7 +21,11 @@ class TestRemovedFields(unittest.TestCase, DbTestBase):
         cls.conn = psycopg.connect(f"service={pgservice}")
 
     def test_dataowner(self):
-        cur = self.conn.cursor(row_factory=psycopg.rows.dict_row)
+        try:
+            cur = self.conn.cursor(row_factory=psycopg.rows.dict_row)
+        except AttributeError:
+            # remove when dropping psycopg2 support
+            cur = self.conn.cursor(cursor_factory=psycopg_extras.DictCursor)
 
         cur.execute("SELECT * FROM tww_od.wastewater_structure LIMIT 1")
         colnames = [desc[0] for desc in cur.description]
