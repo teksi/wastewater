427b04a08ed147881f072fe7478a8add31de6960
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-04-09 14:56:27 +0200

    address review + fixes¨

--- datamodel/scripts/setup.sh
+++ datamodel/scripts/setup.sh
@@ -40,7 +40,4 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/app/symbology_functions
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/app/reach_direction_change.sql -v SRID=$SRID
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/app/14_geometry_functions.sql -v SRID=$SRID
 
-# ${DIR}/app/view/create_views.py --pg_service ${PGSERVICE} --srid ${SRID}
-# psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/app/triggers/network.sql
-
 ${DIR}/app/create_tww_app.py --pg_service ${PGSERVICE} --srid ${SRID}
