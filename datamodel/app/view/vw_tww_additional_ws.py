695c9d72e70f3f2ad1aa4f4b0cf41606a83f8d31
cymed <102035587+cymed@users.noreply.github.com>
2024-04-16 14:06:09 +0200

    rollback

--- datamodel/app/view/vw_tww_additional_ws.py
+++ datamodel/app/view/vw_tww_additional_ws.py
@@ -345,13 +345,13 @@ def vw_tww_additional_ws(srid: int, pg_service: str = None):
       IF OLD.ws_type <> NEW.ws_type THEN
         CASE WHEN OLD.ws_type <> 'unknown' THEN
           BEGIN
-            EXECUTE FORMAT('DELETE FROM tww_od.%I WHERE obj_id = %L',OLD.ws_type,OLD.obj_id);
+            EXECUTE FORMAT('DELETE FROM tww_od.%%I WHERE obj_id = %%L',OLD.ws_type,OLD.obj_id);
           END;
         END CASE;
 
         CASE WHEN NEW.ws_type = ANY(ARRAY['manhole','special_structure','discharge_point','infiltration_installation','drainless_toilet','wwtp_structure','small_treatment_plant']) THEN
           BEGIN
-            EXECUTE FORMAT('INSERT INTO tww_od.%I(obj_id) VALUES (%L)',NEW.ws_type,OLD.obj_id);
+            EXECUTE FORMAT('INSERT INTO tww_od.%%I(obj_id) VALUES (%%L)',NEW.ws_type,OLD.obj_id);
           END;
         END CASE;
       END IF;
