fa4701980d8815b300b31a053f5520678d0d0d93
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2024-05-29 10:59:23 +0200

    Move functions to app
    Move functions to app

--- datamodel/app/create_app.py
+++ datamodel/app/create_app.py
@@ -57,9 +57,10 @@ def create_app(
 
     run_sql("CREATE SCHEMA tww_app;", pg_service)
 
-    run_sql_file("symbology_functions.sql", pg_service)
-    run_sql_file("reach_direction_change.sql", pg_service, variables)
-    run_sql_file("14_geometry_functions.sql", pg_service, variables)
+    run_sql_file("functions/symbology_functions.sql", pg_service)
+    run_sql_file("functions/reach_direction_change.sql", pg_service, variables)
+    run_sql_file("functions/geometry_functions.sql", pg_service, variables)
+    run_sql_file("functions/audit_functions.sql", pg_service, variables)
 
     # open YAML files
     if tww_reach_extra:
