26cabe84852a15c0ebd8b47990806c6ff2f8b685
SJiB <stefan.burckhardt@gmx.ch>
2024-06-25 18:06:29 +0200

    add logger.info

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
@@ -1691,6 +1691,9 @@ class InterlisExporterToIntermediateSchema:
             ).filter(
                 self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
             )
+            # add sql statement to logger
+            statement = query.statement
+            logger.info(f" selection query = {statement}")
         for row in query:
             gebaeudegruppe_baugwr = self.model_classes_interlis.gebaeudegruppe_baugwr(
                 **self.vsa_base_common(row, "gebaeudegruppe_baugwr"),
