fd6752aee4c21f6e41e866316bfd91d8ce02b6c4
SJiB <stefan.burckhardt@gmx.ch>
2024-06-26 20:12:49 +0200

    correct intend

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
@@ -1778,10 +1778,9 @@ class InterlisExporterToIntermediateSchema:
         if self.filtered:
             # to do check if join is ok or left/right join is needed
             query = query.join(
-                    self.model_classes_tww_od.hydr_geometry,
-                    self.model_classes_tww_od.wastewater_node,
-                    )
-                ).filter(
+                self.model_classes_tww_od.hydr_geometry,
+                self.model_classes_tww_od.wastewater_node,
+            ).filter(
                 self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
             )
             # add sql statement to logger
@@ -1948,6 +1947,9 @@ class InterlisExporterToIntermediateSchema:
         query = self.tww_session.query(self.model_classes_tww_od.leapingweir)
         if self.filtered:
             query = query.filter(self.model_classes_tww_od.leapingweir.obj_id.in_(self.subset_ids))
+            # add sql statement to logger
+            statement = query.statement
+            logger.info(f" selection query = {statement}")
         for row in query:
             leapingwehr = self.model_classes_interlis.leapingwehr(
                 **self.overflow_common(row, "leapingwehr"),
