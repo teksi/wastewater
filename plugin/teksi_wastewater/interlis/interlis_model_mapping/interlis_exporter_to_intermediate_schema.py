8408ce3254c9340f138dff33280052d829455b4f
SJiB <stefan.burckhardt@gmx.ch>
2024-06-28 17:46:10 +0200

    correct intend

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
@@ -1274,14 +1274,14 @@ class InterlisExporterToIntermediateSchema:
     def _export_fountain(self):
         query = self.tww_session.query(self.model_classes_tww_od.fountain)
         if self.filtered:
-            # creates duplicate alias fountain / connection_object to wwn without _or
+            # creates duplicate alias fountain / connection_object to wwn without or_
             query = query.join(
                 self.model_classes_tww_od.wastewater_networkelement
                 or_(
                     self.model_classes_tww_od.wastewater_networkelement.obj_id
                     == self.model_classes_tww_od.connection_object.fk_wastewater_networkelement,
                 ),
-                ).filter(
+            ).filter(
                 self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
             )
             # query = query.filter(
