156fb101ae1636b608b47b684c44e0c44e878896
SJiB <stefan.burckhardt@gmx.ch>
2024-07-02 16:39:39 +0200

    add logger exporting selection

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
@@ -72,6 +72,10 @@ class InterlisExporterToIntermediateSchema:
             utils.tww_sqlalchemy.create_engine(), autocommit=False, autoflush=False
         )
 
+        # write self.subset_ids to logger.info
+        if self.filtered:
+            logger.info(f"Exporting selection {self.subset_ids}")
+
         try:
             self._export()
             self.abwasser_session.commit()
@@ -1300,6 +1304,9 @@ class InterlisExporterToIntermediateSchema:
             query = query.join(self.model_classes_tww_od.catchment_area).filter(
                 self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
             )
+            # add sql statement to logger
+            statement = query.statement
+            logger.info(f" selection query = {statement}")
         for row in query:
             ezg_parameter_allg = self.model_classes_interlis.ezg_parameter_allg(
                 **self.surface_runoff_parameters_common(row, "ezg_parameter_allg"),
@@ -1321,6 +1328,9 @@ class InterlisExporterToIntermediateSchema:
             query = query.join(self.model_classes_tww_od.catchment_area).filter(
                 self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
             )
+            # add sql statement to logger
+            statement = query.statement
+            logger.info(f" selection query = {statement}")
         for row in query:
             ezg_parameter_mouse1 = self.model_classes_interlis.ezg_parameter_mouse1(
                 **self.surface_runoff_parameters_common(row, "ezg_parameter_mouse1"),
