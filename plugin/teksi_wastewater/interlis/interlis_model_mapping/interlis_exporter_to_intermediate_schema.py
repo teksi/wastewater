c06718baed67e56dc49c72bae577cbeaf65247ee
SJiB <stefan.burckhardt@gmx.ch>
2024-07-02 14:10:32 +0200

    reactivate filter fountain

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
@@ -1274,19 +1274,19 @@ class InterlisExporterToIntermediateSchema:
     def _export_fountain(self):
         query = self.tww_session.query(self.model_classes_tww_od.fountain)
         if self.filtered:
-            # creates duplicate alias fountain / connection_object to wwn without or_
-            # query = query.join(
-            # self.model_classes_tww_od.wastewater_networkelement,
+            # created duplicate alias fountain / connection_object to wwn without or_ - now changed definition in model_tww_od.py
+            query = query.join(
+            self.model_classes_tww_od.wastewater_networkelement,
             # or_(
             # self.model_classes_tww_od.wastewater_networkelement.obj_id
             # == self.model_classes_tww_od.connection_object.fk_wastewater_networkelement,
-            # ),
-            # ).filter(
-            # self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
-            # )
-            query = query.filter(
-                self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
+            ),
+            ).filter(
+             self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
             )
+            # query = query.filter(
+            #   self.model_classes_tww_od.wastewater_networkelement.obj_id.in_(self.subset_ids)
+            #)
             # add sql statement to logger
             statement = query.statement
             logger.info(f" selection query = {statement}")
