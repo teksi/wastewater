37b6d1d99d3cdd36624adbf105b67705d1120d2d
SJiB <stefan.burckhardt@gmx.ch>
2024-07-08 13:55:48 +0200

    take back in subclasses

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
@@ -274,10 +274,9 @@ class InterlisExporterToIntermediateSchema:
         self._export_bio_ecol_assessment()
         self._check_for_stop()
 
-        # fountain gibt fehler
-        # logger.info("Exporting TWW.fountain -> ABWASSER.Brunnen")
-        # self._export_fountain()
-        # self._check_for_stop()
+        logger.info("Exporting TWW.fountain -> ABWASSER.Brunnen")
+        self._export_fountain()
+        self._check_for_stop()
 
         logger.info("Exporting TWW.param_ca_general -> ABWASSER.EZG_PARAMETER_ALLG")
         self._export_param_ca_general()
@@ -287,10 +286,9 @@ class InterlisExporterToIntermediateSchema:
         self._export_param_ca_mouse1()
         self._check_for_stop()
 
-        # gibt fehler
-        # logger.info("Exporting TWW.individual_surface -> ABWASSER.Einzelflaeche")
-        # self._export_individual_surface()
-        # self._check_for_stop()
+        logger.info("Exporting TWW.individual_surface -> ABWASSER.Einzelflaeche")
+        self._export_individual_surface()
+        self._check_for_stop()
 
         logger.info("Exporting TWW.catchment_area -> ABWASSER.Einzugsgebiet")
         self._export_catchment_area()
@@ -322,9 +320,9 @@ class InterlisExporterToIntermediateSchema:
         self._export_pump()
         self._check_for_stop()
 
-        # logger.info("Exporting TWW.building -> ABWASSER.Gebaeude")
-        # self._export_building()
-        # self._check_for_stop()
+        logger.info("Exporting TWW.building -> ABWASSER.Gebaeude")
+        self._export_building()
+        self._check_for_stop()
 
         logger.info("Exporting TWW.building_group -> ABWASSER.Gebaeudegruppe")
         self._export_building_group()
