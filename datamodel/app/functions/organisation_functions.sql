249947f28202cb7e460f6bb5b831288f475843b3
cymed <102035587+cymed@users.noreply.github.com>
2024-07-19 10:19:20 +0200

    reload latest geometry functions

--- datamodel/app/functions/organisation_functions.sql
+++ datamodel/app/functions/organisation_functions.sql
@@ -1,140 +1,34 @@
-CREATE OR REPLACE FUNCTION tww_app.set_organisations_active()
+CREATE OR REPLACE FUNCTION tww_app.set_organisations_active(_undo_existing boolean default false)
   RETURNS void AS
-  $BODY$
-  BEGIN
-UPDATE tww_od.organisation
+$BODY$
+DECLARE
+schm text;
+tbl text;
+col text;
+BEGIN
+IF _undo_existing THEN
+	UPDATE tww_od.organisation
+	SET tww_active=FALSE;
+ELSE NULL;
+END IF;
+FOR schm,tbl,col IN
+	SELECT   tc.table_schema,
+		tc.table_name,
+		kcu.column_name
+	FROM (SELECT constraint_name FROM information_schema.constraint_column_usage
+	WHERE table_schema='tww_od'
+		AND table_name='organisation'
+		AND column_name='obj_id'
+		AND constraint_name NOT LIKE'pkey%')ccu
+	INNER JOIN information_schema.key_column_usage AS kcu
+	ON ccu.constraint_name = kcu.constraint_name
+	INNER JOIN information_schema.table_constraints AS tc
+		ON tc.constraint_name = kcu.constraint_name
+LOOP
+	        EXECUTE format($$ UPDATE tww_od.organisation
 SET tww_active=TRUE
-WHERE obj_id = ANY(
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.dss15_aquifier
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.building_group
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.building_group_baugwr
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.catchment_area
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.catchment_area_totals
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.connection_object
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.control_center
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.damage
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.data_media
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.disposal
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.farm
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.file
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.hq_relation
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.hydr_geom_relation
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.hydr_geometry
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.hydraulic_char_data
-UNION
-SELECT UNNEST(ARRAY[fk_agency,fk_location_municipality,fk_provider,fk_dataowner])
-FROM tww_od.log_card
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner,fk_operating_company])
-FROM tww_od.maintenance_event
-UNION
-SELECT UNNEST(ARRAY[fk_responsible_entity,fk_responsible_start,fk_provider,fk_dataowner])
-FROM tww_od.measure
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.measurement_result
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.measurement_series
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.measuring_device
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner,fk_operator])
-FROM tww_od.measuring_point
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.mechanical_pretreatment
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.mutation
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.organisation
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.overflow
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.overflow_char
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.pipe_profile
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.profile_geometry
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.reach_point
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.retention_body
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.sludge_treatment
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.structure_part
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.surface_runoff_parameters
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.throttle_shut_off_unit
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.waste_water_treatment
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.waste_water_treatment_plant
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.wastewater_networkelement
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.wastewater_networkelement
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner,fk_owner,fk_operator])
-FROM tww_od.wastewater_structure
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.wwtp_energy_use
-UNION
-SELECT UNNEST(ARRAY[fk_provider,fk_dataowner])
-FROM tww_od.zone
-
-);
+WHERE obj_id IN (SELECT DISTINCT %I FROM %I.%I)$$, col,schm,tbl);
+END LOOP;
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE;
--- datamodel/app/functions/symbology_functions.sql
+++ datamodel/app/functions/symbology_functions.sql
@@ -734,8 +734,11 @@ BEGIN
   FROM tww_od.reach_point rp_to
   WHERE NEW.fk_reach_point_to = rp_to.obj_id;
 
-  NEW.length_effective = COALESCE(sqrt((_rp_from_level - _rp_to_level)^2 + ST_Length(NEW.progression3d_geometry)^2), ST_Length(NEW.progression3d_geometry) );
-
+  IF _rp_from_level=0 OR _rp_to_level=0 THEN
+    NEW.length_effective = ST_Length(NEW.progression3d_geometry);
+  ELSE
+    NEW.length_effective = COALESCE(sqrt((_rp_from_level - _rp_to_level)^2 + ST_Length(NEW.progression3d_geometry)^2), ST_Length(NEW.progression3d_geometry) );
+  END IF;
   RETURN NEW;
 
 END;
@@ -748,9 +751,6 @@ $BODY$
 -----------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION tww_sys.disable_symbology_triggers() RETURNS VOID AS $$
-DECLARE
-    tbl text;
-	trig text;
 BEGIN
   ALTER TABLE tww_od.reach_point DISABLE TRIGGER on_reach_point_update;
   ALTER TABLE tww_od.reach DISABLE TRIGGER on_reach_2_change;
@@ -773,9 +773,6 @@ $$ LANGUAGE plpgsql SECURITY DEFINER;
 -----------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION tww_sys.enable_symbology_triggers() RETURNS VOID AS $$
-DECLARE
-    tbl text;
-	trig text;
 BEGIN
   ALTER TABLE tww_od.reach_point ENABLE TRIGGER on_reach_point_update;
   ALTER TABLE tww_od.reach ENABLE TRIGGER on_reach_2_change;
@@ -789,6 +786,38 @@ BEGIN
   ALTER TABLE tww_od.channel ENABLE TRIGGER ws_symbology_update_by_channel;
   ALTER TABLE tww_od.reach_point ENABLE TRIGGER ws_symbology_update_by_reach_point;
   ALTER TABLE tww_od.reach ENABLE TRIGGER calculate_reach_length;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+-----------------------------------------------------------------------
+-- Check if Symbology Triggers are enabled
+-----------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION tww_sys.check_symbology_triggers_enabled() RETURNS BOOL AS $$
+DECLARE _disabled_count numeric;
+BEGIN
+
+  SELECT count(*) into _disabled_count FROM pg_trigger WHERE (
+    tgname='on_reach_point_update' or
+    tgname='on_reach_2_change' or
+    tgname='on_reach_1_delete' or
+    tgname='on_wastewater_structure_update' or
+    tgname='ws_label_update_by_wastewater_networkelement' or
+    tgname='on_structure_part_change' or
+    tgname='on_cover_change' or
+    tgname='on_wasterwaternode_change' or
+    tgname='ws_symbology_update_by_reach' or
+    tgname='ws_symbology_update_by_channel' or
+    tgname='ws_symbology_update_by_reach_point' or
+    tgname='calculate_reach_length'
+  ) AND tgenabled = 'D';
+
+  IF _disabled_count=0 THEN
+    return true;
+  ELSE
+    return false;
+  END IF;
+
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
