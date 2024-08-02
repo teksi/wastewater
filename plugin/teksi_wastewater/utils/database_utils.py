85cb5b5158c809d1a35542bff28e9f241d94cef8
cymed <102035587+cymed@users.noreply.github.com>
2024-08-02 21:32:37 +0200

    typo

--- plugin/teksi_wastewater/utils/database_utils.py
+++ plugin/teksi_wastewater/utils/database_utils.py
@@ -196,7 +196,7 @@ class DatabaseUtils:
 
         defaults = []
         vals = []
-        for igem in DatabaseUtils.fetchall(
+        for item in DatabaseUtils.fetchall(
             "SELECT fieldname,value_obj_id from tww_od.default_values;"
         ):
             defaults.append(item[0])
