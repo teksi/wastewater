b52ed4d37f63fd68da8a0c6cdbafec0f717e49fb
Cyril Meder <102035587+cymed@users.noreply.github.com>
2024-09-02 11:17:09 +0200

    remove aquifier trigger

--- datamodel/changelogs/0001/51_dss15_aquifer.sql
+++ datamodel/changelogs/0001/51_dss15_aquifer.sql
@@ -35,13 +35,7 @@ ALTER TABLE tww_od.dss15_aquifer ADD COLUMN fk_dataowner varchar(16);
 COMMENT ON COLUMN tww_od.dss15_aquifer.fk_dataowner IS 'Foreignkey to Metaattribute dataowner (as an organisation) - this is the person or body who is allowed to delete, change or maintain this object / Metaattribut Datenherr ist diejenige Person oder Stelle, die berechtigt ist, diesen Datensatz zu löschen, zu ändern bzw. zu verwalten / Maître des données gestionnaire de données, qui est la personne ou l''organisation autorisée pour gérer, modifier ou supprimer les données de cette table/classe';
 ALTER TABLE tww_od.dss15_aquifer ADD COLUMN fk_provider varchar (16);
 COMMENT ON COLUMN tww_od.dss15_aquifer.fk_provider IS 'Foreignkey to Metaattribute provider (as an organisation) - this is the person or body who delivered the data / Metaattribut Datenlieferant ist diejenige Person oder Stelle, die die Daten geliefert hat / FOURNISSEUR DES DONNEES Organisation qui crée l’enregistrement de ces données ';
--------
-CREATE TRIGGER
-update_last_modified_dss15_aquifer
-BEFORE UPDATE OR INSERT ON
- tww_od.dss15_aquifer
-FOR EACH ROW EXECUTE PROCEDURE
- tww_sys.update_last_modified();
+
 
 -------
 
