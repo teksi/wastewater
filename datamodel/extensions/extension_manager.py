0bc393215ec118454aa3b62c7f699336b7240cc4
cymed <102035587+cymed@users.noreply.github.com>
2024-09-02 09:55:57 +0200

    Update datamodel/extensions/extension_manager.py
    Co-authored-by: Philipp Lüthi <philippluca.luethi@gmail.com>

--- datamodel/extensions/extension_manager.py
+++ datamodel/extensions/extension_manager.py
@@ -80,7 +80,7 @@ def load_extension(
         files.sort()
         for file in files:
             filename = os.fsdecode(file)
-            if filename.endswith(".sql") or filename.endswith(".SQL") :
+            if filename.lower().endswith(".sql"):
                 print(f"Running {filename}")
                 run_sql_file(os.path.join(directory, filename), pg_service, variables)
             if filename.endswith(".py"):
