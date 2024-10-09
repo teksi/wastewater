16e6aa3a4137dd7d2a4108801ef41acee5460d27
cymed <102035587+cymed@users.noreply.github.com>
2024-10-09 08:13:04 +0200

    alter variable name
    prefix is a keyword

--- datamodel/app/functions/meta_functions.sql
+++ datamodel/app/functions/meta_functions.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION tww_app.check_all_nulls(rec record, prefix varchar(2),ignored_postfix text[] default Array['obj_id','identifier'])
+CREATE OR REPLACE FUNCTION tww_app.check_all_nulls(rec record, prefix_ varchar(2),ignored_postfix text[] default Array['obj_id','identifier'])
 RETURNS bool
 AS
   $BODY$
@@ -9,8 +9,8 @@ AS
 	  rec_jsonb := (
           SELECT jsonb_object_agg(key, to_jsonb(rec)->key)
           FROM jsonb_object_keys(to_jsonb(rec)) key
-          WHERE LEFT(key, 2) = prefix
-		  AND NOT(ltrim(key, prefix||'_') =ANY(ignored_postfix))
+          WHERE LEFT(key, 2) = prefix_
+		  AND NOT(ltrim(key, prefix_||'_') =ANY(ignored_postfix))
         );
 
       -- Check if all remaining values are NULL
