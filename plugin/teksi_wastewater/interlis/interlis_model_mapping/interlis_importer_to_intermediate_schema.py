8e58081354693fc9a6d45cf096dcde532a1fdb6e
SJiB <stefan.burckhardt@gmx.ch>
2024-10-30 21:03:48 +0100

    Correct  brackets

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_importer_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_importer_to_intermediate_schema.py
@@ -431,7 +431,7 @@ class InterlisImporterToIntermediateSchema:
             # situation3d_geometry=self.session_tww.scalar(ST_Force3D(row.lage, row.kote)),
             #geom = self.session_tww.scalar(ST_Force3D(coteattribute, "Nan"))
             # (COALESCE (NULL,'NaN'))::numeric)
-            geom = self.session_tww.scalar(ST_Force3D(coteattribute, COALESCE (NULL,'NaN'))::numeric)))
+            geom = self.session_tww.scalar(ST_Force3D(coteattribute, COALESCE (NULL,'NaN'))::numeric)
             logger.info(
                 f'No reach_point.cote (Haltungpunkt.Kote) provided for object {obj_id}- situation3d_geometry z-value set to "Nan" instead: {geom}.'
             )
