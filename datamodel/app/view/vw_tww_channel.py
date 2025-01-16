d32b0da30635d234ca2772ac54a72820a6ff7d2c
cymed <102035587+cymed@users.noreply.github.com>
2025-01-16 09:22:17 +0100

    add vw_tww_channel
    I found having a read-only view vw_tww_channel practical, especially when planning maintenance events. The view aggregates all reach geometries linked to a channel.

---
+++ datamodel/app/view/vw_tww_channel.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+#
+# -- View: vw_tww_wastewater_structure
+
+import argparse
+import os
+
+import psycopg2
+from pirogue.utils import select_columns, table_parts
+from yaml import safe_load
+
+
+def vw_tww_channel(pg_service: str = None, extra_definition: dict = None):
+    """
+    Creates tww_channel view
+    :param pg_service: the PostgreSQL service name
+    :param extra_definition: a dictionary for additional read-only columns
+    """
+    if not pg_service:
+        pg_service = os.getenv("PGSERVICE")
+    assert pg_service
+    extra_definition = extra_definition or {}
+
+    conn = psycopg2.connect(f"service={pg_service}")
+    cursor = conn.cursor()
+
+    view_sql = """
+    DROP VIEW IF EXISTS tww_app.vw_tww_channel;
+
+    CREATE OR REPLACE VIEW tww_app.vw_tww_channel AS
+
+    SELECT
+        , {ch_cols}
+        , {ne_cols}
+        , {ws_cols}
+        , ST_LineMerge(ST_Collect(re.progression3d_geometry)) as progression3d_geometry
+        , {extra_cols}
+      FROM tww_od.channel ch
+         LEFT JOIN tww_od.wastewater_structure ws ON ch.obj_id = ws.obj_id
+         LEFT JOIN tww_od.wastewater_networkelement ne ON ne.fk_wastewater_structure = ws.obj_id
+         LEFT JOIN tww_od.reach re ON ne.obj_id = re.obj_id
+         {extra_joins}
+       GROUP BY
+         {ch_cols}
+        , {ne_cols}
+        , {ws_cols}
+        , {extra_cols}
+         ;
+    """.format(
+        extra_cols="\n    , ".join(
+            [
+                select_columns(
+                    pg_cur=cursor,
+                    table_schema=table_parts(table_def["table"])[0],
+                    table_name=table_parts(table_def["table"])[1],
+                    skip_columns=table_def.get("skip_columns", []),
+                    remap_columns=table_def.get("remap_columns", {}),
+                    prefix=table_def.get("prefix", None),
+                    table_alias=table_def.get("alias", None),
+                )
+                for table_def in extra_definition.get("joins", {}).values()
+            ]
+        ),
+        ne_cols=select_columns(
+            pg_cur=cursor,
+            table_schema="tww_od",
+            table_name="wastewater_networkelement",
+            table_alias="ne",
+            remove_pkey=True,
+            indent=4,
+            skip_columns=["fk_wastewater_structure"],
+        ),
+        ch_cols=select_columns(
+            pg_cur=cursor,
+            table_schema="tww_od",
+            table_name="channel",
+            table_alias="ch",
+            prefix="ch_",
+            remove_pkey=True,
+            indent=4,
+            skip_columns=[],
+        ),
+        ws_cols=select_columns(
+            pg_cur=cursor,
+            table_schema="tww_od",
+            table_name="wastewater_structure",
+            table_alias="ws",
+            prefix="ws_",
+            remove_pkey=False,
+            indent=4,
+            skip_columns=[
+                "detail_geometry3d_geometry",
+                "fk_owner",
+                "fk_dataowner",
+                "fk_provider",
+                "_usage_current",
+                "_function_hierarchic",
+                "_label",
+                "_depth",
+                "fk_main_cover",
+            ],
+        ),
+        extra_joins="\n    ".join(
+            [
+                "LEFT JOIN {tbl} {alias} ON {jon}".format(
+                    tbl=table_def["table"],
+                    alias=table_def.get("alias", ""),
+                    jon=table_def["join_on"],
+                )
+                for table_def in extra_definition.get("joins", {}).values()
+            ]
+        ),
+    )
+
+    cursor.execute(view_sql)
+
+    extras = """
+    COMMENT ON VIEW tww_app.vw_tww_channel IS 'Read only';
+    """
+    cursor.execute(extras)
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    # create the top-level parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-e",
+        "--extra-definition",
+        help="YAML file path for extra additions to the view",
+    )
+    parser.add_argument("-p", "--pg_service", help="the PostgreSQL service name")
+    args = parser.parse_args()
+    pg_service = args.pg_service or os.getenv("PGSERVICE")
+    extra_definition = safe_load(open(args.extra_definition)) if args.extra_definition else {}
+    vw_tww_channel(pg_service=pg_service, extra_definition=extra_definition)
