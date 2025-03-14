3dd08fabc4b50710eb733ab363acbd56a783c93a
Cyril Meder <102035587+cymed@users.noreply.github.com>
2025-03-14 11:53:31 +0100

    fix bash and add warning to docs

--- datamodel/scripts/setup.sh
+++ datamodel/scripts/setup.sh
@@ -46,4 +46,4 @@ psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/12_0_roles.sql
 psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ${DIR}/12_1_roles.sql
 
 export PYTHONPATH=${DIR}/app:$PYTHONPATH
-python -m app.create_app.py --pg_service ${PGSERVICE} --srid ${SRID} --extension_names ${EXTENSION_NAMES}
+python -m app.create_app --pg_service ${PGSERVICE} --srid ${SRID} --extension_names ${EXTENSION_NAMES}
