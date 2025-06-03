86d3e65e72c8b62d103394eb067b8f10139d18ae
cymed <102035587+cymed@users.noreply.github.com>
2025-06-03 13:47:35 +0200

    add example for identifier type

--- datamodel/app/extensions/ci_extension/001_demo.sql
+++ datamodel/app/extensions/ci_extension/001_demo.sql
@@ -1,1 +1,10 @@
 SELECT format('%1, my number is %2',{myVariable}, {myNumber});
+
+SELECT re.obj_id,
+    re_mat.{value_lang} AS material,
+    re_mat.{abbr_lang} AS material_abbr
+   FROM tww_od.reach re
+     LEFT JOIN ( SELECT reach_material.code,
+            reach_material.{value_lang},
+            reach_material.abbr_de
+           FROM tww_vl.reach_material) re_mat ON re_mat.code = re.material;
