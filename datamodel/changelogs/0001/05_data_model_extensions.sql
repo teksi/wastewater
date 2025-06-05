53970ceda2212f10b6cc931486c45b35a9349e96
cymed <102035587+cymed@users.noreply.github.com>
2025-06-05 08:37:53 +0200

    revert changes to changelogs

--- datamodel/changelogs/0001/05_data_model_extensions.sql
+++ datamodel/changelogs/0001/05_data_model_extensions.sql
@@ -9,88 +9,21 @@
 ALTER TABLE tww_od.manhole ADD COLUMN _orientation numeric;
 COMMENT ON COLUMN tww_od.manhole._orientation IS 'not part of the VSA-DSS data model
 added solely for TEKSI Wastewater & GEP';
-
-
-ALTER TABLE tww_od.organisation ADD COLUMN tww_active bool DEFAULT FALSE;
-COMMENT ON COLUMN tww_od.organisation.tww_active IS 'not part of the VSA-DSS data model
-added solely for TEKSI Wastewater & GEP
-used to filter organisations';
-
-ALTER TABLE tww_od.organisation ADD COLUMN tww_local_extension bool DEFAULT FALSE;
-COMMENT ON COLUMN tww_od.organisation.tww_local_extension IS 'not part of the VSA-DSS data model
-added solely for TEKSI Wastewater & GEP
-used to map non-harmonized organisations to private on export';
-
-
--- table wastewater_node is extended to hold additional attributes necessary for symbology reasons
--- extended attributes are started with an underscore
--- _usage_current is necessary for coloring the wastewater_node symbols
--- _function_hierarchic is necessary for scale-based filtering (display minor wastewater_nodes only at larger scales)
-
--- TABLE wastewater_node
-CREATE TABLE IF NOT EXISTS tww_od.tww_wastewater_node_symbology
-(
-	fk_wastewater_node character varying(16),
-    _usage_current integer,
-	_function_hierarchic integer,
-	_status integer,
-	CONSTRAINT pkey_tww_od_tww_wastewater_node_symbology_fk_wastewater_node PRIMARY KEY (fk_wastewater_node),
-	CONSTRAINT oorel_od_tww_wastewater_structure_label_wastewater_node FOREIGN KEY (fk_wastewater_node) REFERENCES tww_od.wastewater_node(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
-);
-
-
-COMMENT ON COLUMN tww_od.tww_wastewater_node_symbology._usage_current IS 'not part of the VSA-DSS data model
-added solely for TEKSI Wastewater & GEP
-has to be updated by triggers';
-COMMENT ON COLUMN tww_od.tww_wastewater_node_symbology._function_hierarchic IS 'not part of the VSA-DSS data model
-added solely for TEKSI Wastewater & GEP
-has to be updated by triggers';
-COMMENT ON COLUMN tww_od.tww_wastewater_node_symbology._status IS 'not part of the VSA-DSS data model
-added solely for TEKSI Wastewater & GEP
-has to be updated by triggers';
-
-CREATE TABLE IF NOT EXISTS tww_od.tww_symbology_quarantine
-(
-	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	ws_obj_id character varying(16),
-	wn_obj_id character varying(16),
-	CONSTRAINT unq_tww_symbology_quarantine_ws_obj_id UNIQUE (ws_obj_id),
-	CONSTRAINT unq_tww_symbology_quarantine_wn_obj_id UNIQUE (wn_obj_id)
-);
-
-CREATE TABLE IF NOT EXISTS tww_od.tww_reach_point_label
-(
-	fk_reach_point character varying(16),
-	fk_wastewater_structure character varying(16),
-    label_text text,
-	azimuth numeric(4,3), -- radians
-	CONSTRAINT pkey_tww_od_tww_reach_point_label_fk_reach_point PRIMARY KEY (fk_reach_point),
-	CONSTRAINT oorel_od_tww_reach_point_label_reach_point FOREIGN KEY (fk_reach_point) REFERENCES tww_od.reach_point(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
-	CONSTRAINT fkey_od_tww_reach_point_label_wastewater_structure FOREIGN KEY (fk_wastewater_structure) REFERENCES tww_od.wastewater_structure(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
-);
-
-COMMENT ON TABLE tww_od.tww_reach_point_label IS 'stores reach point labels. not part of the VSA-DSS data model,
-added solely for TEKSI wastewater. has to be updated by triggers';
-
-COMMENT ON COLUMN tww_od.tww_reach_point_label.azimuth IS 'azimuth of respective reach';
-
-CREATE TABLE IF NOT EXISTS tww_od.tww_wastewater_structure_label
-(
-	fk_wastewater_structure character varying(16),
-    label_text_c text,
-	label_text_b text,
-	label_text_rp text,
-	CONSTRAINT pkey_tww_od_tww_wastewater_structure_label_fk_wastewater_structure PRIMARY KEY (fk_wastewater_structure),
-	CONSTRAINT oorel_od_tww_wastewater_structure_label_wastewater_structure FOREIGN KEY (fk_wastewater_structure) REFERENCES tww_od.wastewater_structure(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
-);
-
-COMMENT ON TABLE tww_od.tww_wastewater_structure_label IS 'stores wastewater_structure labels. not part of the VSA-DSS data model,
-added solely for TEKSI wastewater. has to be updated by triggers';
-
-
----------------------------
--- Value List extensions --
----------------------------
+ALTER TABLE tww_od.wastewater_structure ADD COLUMN _label text;
+COMMENT ON COLUMN tww_od.wastewater_structure._label IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP';
+ALTER TABLE tww_od.wastewater_structure ADD COLUMN _cover_label text;
+COMMENT ON COLUMN tww_od.wastewater_structure._cover_label IS 'stores the cover altitude to be used for labelling, not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP';
+ALTER TABLE tww_od.wastewater_structure ADD COLUMN _input_label text;
+COMMENT ON COLUMN tww_od.wastewater_structure._input_label IS 'stores the list of input altitudes to be used for labelling, not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP';
+ALTER TABLE tww_od.wastewater_structure ADD COLUMN _output_label text;
+COMMENT ON COLUMN tww_od.wastewater_structure._output_label IS 'stores the list of output altitudes to be used for labelling, not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP';
+ALTER TABLE tww_od.wastewater_structure ADD COLUMN _bottom_label text;
+COMMENT ON COLUMN tww_od.wastewater_structure._bottom_label IS 'stores the bottom altitude to be used for labelling, not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP';
 
 
 -- this column is an extension to the VSA data model and puts the _function_hierarchic in order
@@ -132,12 +65,6 @@ ALTER TABLE tww_vl.channel_function_hierarchic ADD COLUMN tww_use_in_labels bool
 UPDATE tww_vl.channel_function_hierarchic
 SET tww_use_in_labels= true WHERE value_en like 'pwwf%';
 
-
--- this column is an extension to the VSA data model defines which status to use in labels
-ALTER TABLE tww_vl.wastewater_structure_status ADD COLUMN tww_use_in_labels bool DEFAULT false;
-UPDATE tww_vl.wastewater_structure_status
-SET tww_use_in_labels= true WHERE code=ANY('{8493,6530,6533}');
-
 -- this column is an extension to the VSA data model defines which inflow usage_current is prioritised over the outflow
 ALTER TABLE tww_vl.channel_usage_current ADD COLUMN tww_symbology_inflow_prio bool DEFAULT false;
 UPDATE tww_vl.channel_usage_current
@@ -164,3 +91,34 @@ SET tww_symbology_order=
 		 ,5322 --unknown
 		 ]
 	 ,code);
+
+
+
+-- table wastewater_node is extended to hold additional attributes necessary for symbology reasons
+-- extended attributes are started with an underscore
+-- _usage_current is necessary for coloring the wastewater_node symbols
+-- _function_hierarchic is necessary for scale-based filtering (display minor wastewater_nodes only at larger scales)
+
+-- TABLE wastewater_node
+ALTER TABLE tww_od.wastewater_node ADD COLUMN _usage_current integer;
+COMMENT ON COLUMN tww_od.wastewater_node._usage_current IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+has to be updated by triggers';
+ALTER TABLE tww_od.wastewater_node ADD COLUMN _function_hierarchic integer;
+COMMENT ON COLUMN tww_od.wastewater_node._function_hierarchic IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+has to be updated by triggers';
+ALTER TABLE tww_od.wastewater_node ADD COLUMN _status integer;
+COMMENT ON COLUMN tww_od.wastewater_node._status IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+has to be updated by triggers';
+
+ALTER TABLE tww_od.organisation ADD COLUMN tww_active bool DEFAULT FALSE;
+COMMENT ON COLUMN tww_od.organisation.tww_active IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+used to filter organisations';
+
+ALTER TABLE tww_od.organisation ADD COLUMN tww_local_extension bool DEFAULT FALSE;
+COMMENT ON COLUMN tww_od.organisation.tww_local_extension IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+used to map non-harmonized organisations to private on export';
---
+++ datamodel/update/delta_2025.0.1_separate_labels.sql
@@ -0,0 +1,62 @@
+CREATE TABLE IF NOT EXISTS tww_od.tww_wastewater_node_symbology
+(
+	fk_wastewater_node character varying(16),
+    _usage_current integer,
+	_function_hierarchic integer,
+	_status integer,
+	CONSTRAINT pkey_tww_od_tww_wastewater_node_symbology_fk_wastewater_node PRIMARY KEY (fk_wastewater_node),
+	CONSTRAINT oorel_od_tww_wastewater_structure_label_wastewater_node FOREIGN KEY (fk_wastewater_node) REFERENCES tww_od.wastewater_node(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
+);
+
+
+COMMENT ON COLUMN tww_od.tww_wastewater_node_symbology._usage_current IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+has to be updated by triggers';
+COMMENT ON COLUMN tww_od.tww_wastewater_node_symbology._function_hierarchic IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+has to be updated by triggers';
+COMMENT ON COLUMN tww_od.tww_wastewater_node_symbology._status IS 'not part of the VSA-DSS data model
+added solely for TEKSI Wastewater & GEP
+has to be updated by triggers';
+
+CREATE TABLE IF NOT EXISTS tww_od.tww_symbology_quarantine
+(
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	ws_obj_id character varying(16),
+	wn_obj_id character varying(16),
+	CONSTRAINT unq_tww_symbology_quarantine_ws_obj_id UNIQUE (ws_obj_id),
+	CONSTRAINT unq_tww_symbology_quarantine_wn_obj_id UNIQUE (wn_obj_id)
+);
+
+CREATE TABLE IF NOT EXISTS tww_od.tww_reach_point_label
+(
+	fk_reach_point character varying(16),
+	fk_wastewater_structure character varying(16),
+    label_text text,
+	azimuth numeric(4,3), -- radians
+	CONSTRAINT pkey_tww_od_tww_reach_point_label_fk_reach_point PRIMARY KEY (fk_reach_point),
+	CONSTRAINT oorel_od_tww_reach_point_label_reach_point FOREIGN KEY (fk_reach_point) REFERENCES tww_od.reach_point(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+	CONSTRAINT fkey_od_tww_reach_point_label_wastewater_structure FOREIGN KEY (fk_wastewater_structure) REFERENCES tww_od.wastewater_structure(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
+);
+
+COMMENT ON TABLE tww_od.tww_reach_point_label IS 'stores reach point labels. not part of the VSA-DSS data model,
+added solely for TEKSI wastewater. has to be updated by triggers';
+
+COMMENT ON COLUMN tww_od.tww_reach_point_label.azimuth IS 'azimuth of respective reach';
+
+CREATE TABLE IF NOT EXISTS tww_od.tww_wastewater_structure_label
+(
+	fk_wastewater_structure character varying(16),
+    label_text_c text,
+	label_text_b text,
+	label_text_rp text,
+	CONSTRAINT pkey_tww_od_tww_wastewater_structure_label_fk_wastewater_structure PRIMARY KEY (fk_wastewater_structure),
+	CONSTRAINT oorel_od_tww_wastewater_structure_label_wastewater_structure FOREIGN KEY (fk_wastewater_structure) REFERENCES tww_od.wastewater_structure(obj_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
+);
+
+COMMENT ON TABLE tww_od.tww_wastewater_structure_label IS 'stores wastewater_structure labels. not part of the VSA-DSS data model,
+added solely for TEKSI wastewater. has to be updated by triggers';
+
+ALTER TABLE tww_od.wastewater_node DROP COLUMN IF EXISTS _usage_current;
+ALTER TABLE tww_od.wastewater_node DROP COLUMN IF EXISTS _function_hierarchic;
+ALTER TABLE tww_od.wastewater_node DROP COLUMN IF EXISTS _status;
