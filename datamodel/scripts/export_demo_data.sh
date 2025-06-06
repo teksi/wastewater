e348701c3a8f033e129ccacd43cb4897960dfec4
Damiano Lombardi <damiano@opengis.ch>
2025-06-06 23:15:50 +0200

    Add script to create and export demo data from test dataset interlis

---
+++ datamodel/scripts/export_demo_data.sh
@@ -0,0 +1,10 @@
+
+docker compose exec db dropdb -U postgres tww
+docker compose exec db createdb -U postgres tww
+
+docker compose exec db pum -vvv -s pg_tww -d datamodel install -p SRID 2056 --roles --grant
+
+docker compose run qgis sh -c 'xvfb-run /usr/src/plugin/tww_cmd.py interlis_import --xtf_file /usr/src/plugin/teksi_wastewater/tests/data/test-dataset-organisations.xtf --pghost db --pgdatabase tww --pguser postgres --pgpass postgres --pgport 5432'
+docker compose run qgis sh -c 'xvfb-run /usr/src/plugin/tww_cmd.py interlis_import --xtf_file /usr/src/plugin/teksi_wastewater/tests/data/test-dataset-DSS.xtf --pghost db --pgdatabase tww --pguser postgres --pgpass postgres --pgport 5432'
+
+docker compose exec db pg_dump --inserts --data-only --schema=tww_od -U postgres tww > demo_data.sql
