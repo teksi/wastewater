76c9a0372ccc59315fd6c4717e8357b4de1ae964
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-06-06 22:14:02 +0200

    remove obsolete role files2

--- datamodel/roles/roles_drop.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-DECLARE
-    role text;
-BEGIN
-    FOREACH role IN ARRAY ARRAY['tww_viewer', 'tww_user', 'tww_manager', 'tww_sysadmin'] LOOP
-      DROP ROLE IF EXISTS role;
-    END LOOP;
-END
-$$;
