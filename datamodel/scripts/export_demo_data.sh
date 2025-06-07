e9bbfb48de896ec0abcbded2b37aa94b44c40f1a
Damiano Lombardi <damiano@opengis.ch>
2025-06-07 10:50:17 +0200

    Remove set_config search path

--- datamodel/scripts/export_demo_data.sh
+++ datamodel/scripts/export_demo_data.sh
@@ -7,4 +7,7 @@ docker compose exec db pum -vvv -s pg_tww -d datamodel install -p SRID 2056 --ro
 docker compose run qgis sh -c 'xvfb-run /usr/src/plugin/tww_cmd.py interlis_import --xtf_file /usr/src/plugin/teksi_wastewater/tests/data/test-dataset-organisations.xtf --pghost db --pgdatabase tww --pguser postgres --pgpass postgres --pgport 5432'
 docker compose run qgis sh -c 'xvfb-run /usr/src/plugin/tww_cmd.py interlis_import --xtf_file /usr/src/plugin/teksi_wastewater/tests/data/test-dataset-DSS.xtf --pghost db --pgdatabase tww --pguser postgres --pgpass postgres --pgport 5432'
 
-docker compose exec db pg_dump --inserts --data-only --schema=tww_od -U postgres tww > demo_data.sql
+docker compose exec db pg_dump --inserts --data-only --no-owner --no-privileges --schema=tww_od -U postgres tww > demo_data.sql
+
+# Remove select set_config
+sed -i '/^SELECT pg_catalog\.set_config/d' demo_data.sql
