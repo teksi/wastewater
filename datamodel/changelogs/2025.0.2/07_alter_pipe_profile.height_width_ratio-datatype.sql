6db88f9613a8aa8090c8d45c044fd33e0045b837
Stefan <stefan.burckhardt@gmx.ch>
2025-07-16 22:31:00 +0200

    add TYPE

--- datamodel/changelogs/2025.0.2/07_alter_pipe_profile.height_width_ratio-datatype.sql
+++ datamodel/changelogs/2025.0.2/07_alter_pipe_profile.height_width_ratio-datatype.sql
@@ -1,3 +1,3 @@
 -- ALTER TABLE tww_od.pipe_profile ADD COLUMN height_width_ratio  decimal(5,2) ;
 
- ALTER TABLE tww_od.pipe_profile ALTER COLUMN height_width_ratio numeric(8,5);
+ ALTER TABLE tww_od.pipe_profile ALTER COLUMN height_width_ratio TYPE numeric(8,5);
