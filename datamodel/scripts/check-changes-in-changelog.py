59aa7a1b1a2e5bdddd4043bb48358f93e95a6531
Cyril Meder <102035587+cymed@users.noreply.github.com>
2025-07-17 19:07:17 +0200

    Update check-changes-in-changelog.py

--- datamodel/scripts/check-changes-in-changelog.py
+++ datamodel/scripts/check-changes-in-changelog.py
@@ -67,5 +67,4 @@ if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
 
-    files = compare_versions(version=args.version, database=args.database)
-    print("Dumps created: {}".format(", ".join(files)))
+    files = compare_versions(latest_stable_release=args.latest_stable_release, changed_files=args.changed_files)
