3b7396f8752a6d3a4fb082e6d60ddab57ecd8f32
Stefan <stefan.burckhardt@gmx.ch>
2025-07-18 15:40:16 +0200

    edit assertEqual value to 1.130 for the moment

--- plugin/teksi_wastewater/tests/test_interlis.py
+++ plugin/teksi_wastewater/tests/test_interlis.py
@@ -119,7 +119,9 @@ class TestInterlis(unittest.TestCase):
             "SELECT height_width_ratio FROM tww_od.pipe_profile WHERE obj_id='ch000000PP000003';"
         )
         self.assertIsNotNone(result)
-        self.assertEqual(result[0], 1.12857)
+        self.assertEqual(result[0], 1.130)
+        # in future if VSA-DSS / SIA405 INTERLIS is also patched change to:
+        # self.assertEqual(result[0], 1.12857)
 
         # Import minimal dss
         xtf_file_input = self._get_data_filename(MINIMAL_DATASET_DSS)
@@ -203,7 +205,7 @@ class TestInterlis(unittest.TestCase):
         xml_height_width_ratio = interlis_object.attrib.get("height_width_ratio", None)
         self.assertIsNotNone(xml_height_width_ratio)
         self.assertEqual(xml_height_width_ratio, 1.30)
-        # in future if VSA-DSS / SIA405 is patched
+        # in future if VSA-DSS / SIA405 INTERLIS is also patched  change to:
         # self.assertEqual(xml_height_width_ratio, 1.12857)
 
         # Export minimal dss
