bca3df0c30d4f677b3c52d6087b443ac3f6d7af3
Stefan <stefan.burckhardt@gmx.ch>
2025-08-05 17:29:38 +0200

    rename 52 and only insert / updates

--- datamodel/changelogs/2025.0.2/52_extend_update_tww_vl_tables_description_display_vsa_kek.sql
+++ datamodel/changelogs/2025.0.2/52_extend_update_tww_vl_tables_description_display_vsa_kek.sql
@@ -2,19 +2,6 @@
 ------ For questions etc. please contact Stefan Burckhardt stefan.burckhardt@sjib.ch
 ------ version 07.07.2025 18:10:38
 
-ALTER TABLE tww_vl.value_list_base
-ADD COLUMN IF NOT EXISTS description_en text,
-ADD COLUMN IF NOT EXISTS description_de text,
-ADD COLUMN IF NOT EXISTS description_fr text,
-ADD COLUMN IF NOT EXISTS description_it text,
-ADD COLUMN IF NOT EXISTS description_ro text,
-ADD COLUMN IF NOT EXISTS display_en character varying(100),
-ADD COLUMN IF NOT EXISTS display_de character varying(100),
-ADD COLUMN IF NOT EXISTS display_fr character varying(100),
-ADD COLUMN IF NOT EXISTS display_it character varying(100),
-ADD COLUMN IF NOT EXISTS display_ro character varying(100);
-
-
 --- Adapt tww_vl.examination_recording_type
  INSERT INTO tww_vl.examination_recording_type (code, vsacode) VALUES (3681,3681) ON CONFLICT DO NOTHING;
 
