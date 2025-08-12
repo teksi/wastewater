3011dda392e275127348269a7cd5ed9de205f5ff
Stefan <stefan.burckhardt@gmx.ch>
2025-08-12 15:56:13 +0200

    with namespace

--- plugin/teksi_wastewater/tests/test_interlis.py
+++ plugin/teksi_wastewater/tests/test_interlis.py
@@ -288,11 +288,15 @@ class TestInterlis(unittest.TestCase):
         # xml_height_width_ratio = interlis_object.find("HoehenBreitenverhaeltnis")
 
         # rank = country.find('rank').text
-        xml_height_width_ratio = interlis_object.findall("HoehenBreitenverhaeltnis")
+        # xml_height_width_ratio = interlis_object.findall("HoehenBreitenverhaeltnis")
+        xmlns = {"": "http://www.interlis.ch/INTERLIS2.3"} # kein namespace Prefix
+        xml_height_width_ratio = interlis_object.findall("HoehenBreitenverhaeltnis", xmlns)
+
         if len(xml_height_width_ratio) > 0:
             xml_height_width_ratio_value = xml_height_width_ratio[0]
         else:
             print ("No HoehenBreitenverhaeltnis found!")
+            xml_height_width_ratio_value = 0
 
         self.assertEqual(xml_height_width_ratio_value, 1.13)
 
