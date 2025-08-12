e6a61cc0436200f8e8eb910032d3fb8db732b587
Stefan <stefan.burckhardt@gmx.ch>
2025-08-12 11:22:54 +0200

    take out TRANSFER

--- plugin/teksi_wastewater/tests/test_interlis.py
+++ plugin/teksi_wastewater/tests/test_interlis.py
@@ -109,8 +109,9 @@ class TestInterlis(unittest.TestCase):
 
                 # 12.8.2025 try adding .format(namespace, topicname, classname)
                 # only use namespace as {0}
+                # take out TRANSFER
                 xml_attribute = root.findall(
-                    "./TRANSFER/{0}DATASECTION/{0}SIA405_ABWASSER_2020_1_LV95.SIA405_Abwasser/{0}SIA405_ABWASSER_2020_1_LV95.SIA405_Abwasser.Rohrprofil[@TID='ch000000PP000003']/HoehenBreitenverhaeltnis/text()".format(
+                    "./{0}DATASECTION/{0}SIA405_ABWASSER_2020_1_LV95.SIA405_Abwasser/{0}SIA405_ABWASSER_2020_1_LV95.SIA405_Abwasser.Rohrprofil[@TID='ch000000PP000003']/HoehenBreitenverhaeltnis/text()".format(
                         namespace
                     )
                 )
