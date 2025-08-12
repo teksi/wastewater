463a0a4c36bdb167581548aaf7c2b924ebd7e31f
cymed <102035587+cymed@users.noreply.github.com>
2025-08-12 13:44:32 +0200

    comments

--- datamodel/changelogs/2025.0.2/80_rename_mr_time_value.sql
+++ datamodel/changelogs/2025.0.2/80_rename_mr_time_value.sql
@@ -1,2 +1,2 @@
-ALTER TABLE tww_od.measurement_result RENAME COLUMN "time" TO time_point;
-ALTER TABLE tww_od.measurement_result RENAME COLUMN "value" TO measurement_value;
+ALTER TABLE tww_od.measurement_result RENAME COLUMN "time" TO time_point; --renamed 20250812 as time is a reserved SQL:2023 keyword
+ALTER TABLE tww_od.measurement_result RENAME COLUMN "value" TO measurement_value; -- #renamed 20250812 as value is a reserved SQL:2023 keyword
