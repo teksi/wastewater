ac71205e5ae194468b91a739be2506e86c85b73c
cymed <102035587+cymed@users.noreply.github.com>
2025-10-06 16:51:20 +0200

    fix #757

--- plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
+++ plugin/teksi_wastewater/interlis/interlis_model_mapping/interlis_exporter_to_intermediate_schema.py
@@ -90,6 +90,7 @@ class InterlisExporterToIntermediateSchema:
             self.abwasser_session.commit()
             self.close_sessions()
         except Exception as e:
+            enhanced_exc = NotImplementedError
             if hasattr(e, "pgcode") and e.pgcode == "23503":  # psycopg2/3
                 enhanced_exc = self.parse_fk_violation(e)
             self.close_sessions()
