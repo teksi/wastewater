69e227d41be08c643200db4347245fbfcd2736ab
cymed <102035587+cymed@users.noreply.github.com>
2025-10-21 10:48:28 +0200

    cover._depth trigger funcs

--- datamodel/app/sql_functions/geometry_functions.sql
+++ datamodel/app/sql_functions/geometry_functions.sql
@@ -7,7 +7,21 @@
 CREATE OR REPLACE FUNCTION tww_app.synchronize_level_with_altitude_on_wastewater_node()
   RETURNS trigger AS
 $BODY$
+DECLARE 
+  co_obj_ids varchar(16)[];
+  min_level numeric;
+  co_obj_id varchar(16);
 BEGIN
+  SELECT array_agg(co.obj_id), min(wn.bottom_level) INTO co_obj_ids,min_level 
+  FROM tww_od.cover co
+  JOIN tww_od.structure_part sp on sp.obj_id=co.obj_id
+  JOIN tww_od.wastewater_networkelement ne on sp.fk_wastewater_structure=ne.fk_wastewater_structure AND ne.obj_id=NEW.obj_id
+  JOIN tww_od.wastewater_node wn on wn.obj_id=ne.obj_id;
+
+  FOR co_obj_id IN ARRAY co_obj_ids LOOP
+    EXECUTE FORMAT("UPDATE tww_od.cover SET _depth = level - %s$1 WHERE obj_id = %L$2",min_level,co_obj_id)
+  END LOOP;
+
   CASE
     WHEN TG_OP = 'INSERT' THEN
       NEW.situation3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.situation3d_geometry), ST_Y(NEW.situation3d_geometry), COALESCE(NEW.bottom_level,'NaN') ), {SRID});
@@ -43,16 +57,25 @@ CREATE TRIGGER synchronize_level_with_altitude
 CREATE OR REPLACE FUNCTION tww_app.synchronize_level_with_altitude_on_cover()
   RETURNS trigger AS
 $BODY$
+DECLARE 
+wn_level numeric;
 BEGIN
+  SELECT min(wn.bottom_level) INTO wn_level 
+  FROM tww_od.wastewater_structure ws
+  JOIN tww_od.structure_part sp on sp.fk_wastewater_structure=ws.obj_id AND sp.obj_id=NEW.obj_id
+  JOIN tww_od.wastewater_node wn ON ws.fk_main_wastewater_node = wn.obj_id;
+
   CASE
     WHEN TG_OP = 'INSERT' THEN
       NEW.situation3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.situation3d_geometry), ST_Y(NEW.situation3d_geometry), COALESCE(NEW.level,'NaN') ), {SRID});
+      NEW._depth = NEW.level - wn_level
     WHEN TG_OP = 'UPDATE' THEN
       IF NEW.level <> OLD.level OR (NEW.level IS NULL AND OLD.level IS NOT NULL) OR (NEW.level IS NOT NULL AND OLD.level IS NULL) THEN
         NEW.situation3d_geometry = ST_SetSRID( ST_MakePoint( ST_X(NEW.situation3d_geometry), ST_Y(NEW.situation3d_geometry), COALESCE(NEW.level,'NaN') ), {SRID});
       ELSE
         IF ST_Z(NEW.situation3d_geometry) <> ST_Z(OLD.situation3d_geometry) THEN
           NEW.level = NULLIF(ST_Z(NEW.situation3d_geometry),'NaN');
+          NEW._depth = NEW.level - wn_level
         END IF;
       END IF;
   END CASE;
