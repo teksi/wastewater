a13e68c93f324103e4917bb78821b5f28a0ac0e7
Stefan <stefan.burckhardt@gmx.ch>
2026-01-23 21:02:03 +0100

    Adapting to 1.14.8

--- plugin/teksi_wastewater/interlis/config.py
+++ plugin/teksi_wastewater/interlis/config.py
@@ -2,7 +2,7 @@ import os
 
 BASE = os.path.dirname(__file__)
 
-ILIVALIDATOR = os.path.join(BASE, "bin", "ilivalidator-1.14.7", "ilivalidator-1.14.7.jar")
+ILIVALIDATOR = os.path.join(BASE, "bin", "ilivalidator-1.14.8", "ilivalidator-1.14.8.jar")
 
 TWW_DEFAULT_PGSERVICE = "pg_tww"
 TWW_OD_SCHEMA = "tww_od"
