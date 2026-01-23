799c130ad63dfe83b747e2eb96564b6a090a09bc
Stefan <stefan.burckhardt@gmx.ch>
2026-01-23 16:35:18 +0100

    "" added

--- plugin/teksi_wastewater/interlis/utils/ili2db.py
+++ plugin/teksi_wastewater/interlis/utils/ili2db.py
@@ -84,9 +84,9 @@ class InterlisTools:
                 [
                     f'"{self.java_executable_path}"',
                     "-jar ",
-                    {config.ILIVALIDATOR},
+                    ""{config.ILIVALIDATOR}"",
                     "--refdata ",
-                    {config.EXTERNAL_ORGANISATION},
+                    ""{config.EXTERNAL_ORGANISATION}"",
                     "--allObjectsAccessible",
                     "--log",
                     f'"{log_path}"',
