ea7c05807e66b48dbfaad24737c60069aa4c05bf
Cyril Meder <c.meder@wapa.ch>
2026-02-20 12:33:31 +0100

    fix

--- datamodel/changelogs/2026.0.1/04_add_indexes_for_main_views.sql
+++ datamodel/changelogs/2026.0.1/04_add_indexes_for_main_views.sql
@@ -10,6 +10,6 @@ CREATE INDEX in_wastewater_structure_fk_main_wastewater_node ON tww_od.wastewate
 CREATE INDEX in_wastewater_structure_fk_main_cover ON tww_od.wastewater_structure USING btree (fk_main_cover);
 CREATE INDEX in_wastewater_structure_fk_owner ON tww_od.wastewater_structure USING btree (fk_owner);
 
-DROP INDEX IF EXISTS in_channel_function_hierarchic_usage_current ON tww_od.channel USING btree (function_hierarchic, usage_current);
+DROP INDEX IF EXISTS in_channel_function_hierarchic_usage_current; -- ON tww_od.channel USING btree (function_hierarchic, usage_current)
 CREATE INDEX in_channel_usage_current ON tww_od.channel USING btree (usage_current);
 CREATE INDEX in_channel_function_hierarchic ON tww_od.channel USING btree (function_hierarchic);
