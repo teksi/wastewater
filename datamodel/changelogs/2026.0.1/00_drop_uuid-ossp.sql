396e6e7c082cc1f9d4b24fbcd492313c31356389
Arnaud Poncet-Montanges <ponceta@users.noreply.github.com>
2026-04-02 13:12:15 +0200

    Comment the drop extension to avoid db scope extension removal

--- datamodel/changelogs/2026.0.1/00_drop_uuid-ossp.sql
+++ datamodel/changelogs/2026.0.1/00_drop_uuid-ossp.sql
@@ -1,11 +1,11 @@
---Alteration of changelog 2025.0.1/03_tww_db_dss.sql
+-- Alteration of changelog 2025.0.1/03_tww_db_dss.sql
 ALTER TABLE tww_od.re_building_group_disposal
 ALTER COLUMN id SET DEFAULT gen_random_uuid();
 
 ALTER TABLE tww_od.re_maintenance_event_wastewater_structure
 ALTER COLUMN id SET DEFAULT gen_random_uuid();
 
---Alteration of changelog 2025.0.2/07_tww_db_dss.sql
+-- Alteration of changelog 2025.0.2/07_tww_db_dss.sql
 ALTER TABLE tww_od.agxx_wastewater_node
 ALTER COLUMN uuid SET DEFAULT gen_random_uuid();
 
@@ -36,5 +36,10 @@ ALTER COLUMN uuid SET DEFAULT gen_random_uuid();
 ALTER TABLE tww_od.agxx_last_modification
 ALTER COLUMN uuid SET DEFAULT gen_random_uuid();
 
---Remove extension / Alteration of changelog 2025.0.1/00_db_extensions.sql
-DROP EXTENSION IF EXISTS "uuid-ossp";
+-- Remove extension / Alteration of changelog 2025.0.1/00_db_extensions.sql
+-- DROP EXTENSION IF EXISTS "uuid-ossp";
+/* This has been commented since it affects the whole DB
+   With this delta, system will run on both pg 14+ version
+   and pg<=13 with the extension uuid-ossp
+   Users can still remove the extension manually if they want
+*/
