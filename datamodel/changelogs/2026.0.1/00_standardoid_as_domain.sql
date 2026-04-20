f7d5fcb7385ab9789b2ba73f46e9853ffd02b1e4
cymed <102035587+cymed@users.noreply.github.com>
2026-04-20 15:37:01 +0200

    break if there are non-conform obj_id

--- datamodel/changelogs/2026.0.1/00_standardoid_as_domain.sql
+++ datamodel/changelogs/2026.0.1/00_standardoid_as_domain.sql
@@ -8,6 +8,46 @@ CHECK (
 DO $$
 DECLARE
     r record;
+    cnt bigint;
+BEGIN
+    FOR r IN
+        SELECT n.nspname, c.relname
+        FROM pg_class c
+        JOIN pg_namespace n ON n.oid = c.relnamespace
+        JOIN pg_attribute a ON a.attrelid = c.oid
+        WHERE n.nspname = 'tww_od'
+          AND c.relkind = 'r'
+          AND a.attname = 'obj_id'
+    LOOP
+        EXECUTE format(
+            'SELECT count(*) FROM %I.%I
+             WHERE obj_id IS NULL
+                OR length(obj_id) <> 16
+                OR obj_id !~ ''^[A-Za-z0-9]{16}$''',
+            r.nspname,
+            r.relname
+        ) INTO cnt;
+
+        IF cnt > 0 THEN
+            RAISE EXCEPTION
+                USING
+                    MESSAGE = format(
+                        'Aborting upgrade: %I.%I has %s non-confore obj_id values.',
+                        r.nspname,
+                        r.relname,
+                        cnt
+                    ),
+                    ERRCODE = 'check_violation';
+        END IF;
+    END LOOP;
+END;
+$$;
+
+
+
+DO $$
+DECLARE
+    r record;
 BEGIN
     FOR r IN
         SELECT
