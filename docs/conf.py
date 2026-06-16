a84aa056895e3ba1c54c13b3d3e277204f089ee5
Cyril Meder <c.meder@wapa.ch>
2026-06-16 15:22:30 +0200

    fix master doc

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ source_suffix = ".rst"
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = "index"
+master_doc = "en/index"
 
 # General information about the project.
 project = "TWW"
