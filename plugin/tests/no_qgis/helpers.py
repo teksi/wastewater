92df99dcb5ec2d78b84c926d658674adf383e68e
Cyril Meder <c.meder@wapa.ch>
2026-07-22 15:00:53 +0200

    fixes

--- plugin/tests/no_qgis/helpers.py
+++ plugin/tests/no_qgis/helpers.py
@@ -4,8 +4,11 @@ from subprocess import run as sp_run
 def run_cli(command: str):
     cmd = f"""
     docker compose exec qgis sh -c '
-    xvfb-run python /usr/src/plugin/tww_cmd.py {command}
+    xvfb-run python3 /usr/src/plugin/tww_cmd.py {command}
     '
     """
-    result = sp_run(cmd, shell=True)
+
+    result = sp_run(cmd, shell=True, capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
     assert result.returncode == 0
