2099d6dc1fc25521c6e824fa8503a1d195ced369
Cyril Meder <c.meder@wapa.ch>
2026-07-24 07:54:08 +0200

    debug messages

--- plugin/tww_cmd.py
+++ plugin/tww_cmd.py
@@ -207,6 +207,10 @@ class TeksiWastewaterCmd:
         selected_ids = []
         if self.args.selected_ids:
             selected_ids = self.args.selected_ids.split(",")
+            print(f"self.args.selected_ids = {self.args.selected_ids!r}") # debug only
+            print(f"selected_ids = {selected_ids!r}")
+        else:
+            print("No selection argument. Exporting the whole dataset.")
 
         interlisImporterExporter = InterlisImporterExporter()
         try:
