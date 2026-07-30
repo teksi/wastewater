d54c3ae3b85264e0b7dba5c1ede57e783463c0df
Stefan <stefan.burckhardt@gmx.ch>
2026-07-30 14:49:44 +0200

    Add display_language columns

---
+++ datamodel/changelogs/2026.0.2/03_alter_dictionary_od_table_display_colums.sql
@@ -0,0 +1,6 @@
+ALTER TABLE tww_sys.dictionary_od_table ALTER COLUMN display_en text;
+ALTER TABLE tww_sys.dictionary_od_table ALTER COLUMN display_de text;
+ALTER TABLE tww_sys.dictionary_od_table ALTER COLUMN display_fr text;
+ALTER TABLE tww_sys.dictionary_od_table ALTER COLUMN display_it text;
+ALTER TABLE tww_sys.dictionary_od_table ALTER COLUMN display_ro text;
+
