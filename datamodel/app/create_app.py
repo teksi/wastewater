19f9908d2ad9b73da0bc9d719983123ad9266d8c
Cyril Meder <c.meder@wapa.ch>
2026-08-10 16:22:13 +0200

    alter create order

--- datamodel/app/create_app.py
+++ datamodel/app/create_app.py
@@ -162,6 +162,15 @@ Running modification {modification.get('id')}
             if value:
                 self.extra_definitions[key] = self.abspath / value
 
+        vw_tww_wastewater_node(
+            connection=connection,
+            srid=SRID,
+            extra_definition=(
+                self.load_yaml(self.extra_definitions["vw_tww_wastewater_node"])
+                if self.extra_definitions.get("vw_tww_wastewater_node")
+                else {}
+            ),
+        )
         vw_wastewater_structure(
             connection=self._connection,
             extra_definition=(
@@ -176,15 +185,6 @@ Running modification {modification.get('id')}
             extra_definition=(
                 self.load_yaml(self.extra_definitions["vw_tww_wastewater_structure"])
                 if self.extra_definitions.get("vw_tww_wastewater_structure")
-                else {}
-            ),
-        )
-        vw_tww_wastewater_node(
-            connection=connection,
-            srid=SRID,
-            extra_definition=(
-                self.load_yaml(self.extra_definitions["vw_tww_wastewater_node"])
-                if self.extra_definitions.get("vw_tww_wastewater_node")
                 else {}
             ),
         )
